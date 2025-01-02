356594a5e77cce4423a693308481649e2b816b46
ume <chi.nguyenthiyen.203@gmail.com>
2025-01-02 16:37:46 +0700

    Add some Buttons with function

--- app/components/Camera.tsx
+++ app/components/Camera.tsx
@@ -3,7 +3,7 @@
 import { ModeToggle } from "@/components/theme-toggle";
 import { Button } from "@/components/ui/button";
 import { Separator } from "@/components/ui/separator";
-import { FlipHorizontal } from "lucide-react";
+import { CameraIcon, FlipHorizontal, Video } from "lucide-react";
 import React, { useRef, useState } from "react";
 import Webcam from "react-webcam";
 
@@ -13,7 +13,8 @@ export default function Camera(props: Props) {
   const webcamRef = useRef<Webcam>(null);
   const canvasRef = useRef<HTMLCanvasElement>(null);
 
-  const [mirrored, setMirrored] = useState<boolean>(false);
+  const [mirrored, setMirrored] = useState<boolean>(true);
+  const [isRecording, setIsRecording] = useState<boolean>(false);
 
   return (
     <div>
@@ -35,24 +36,82 @@ export default function Camera(props: Props) {
         {/* Tracking Details */}
         <div className="flex flex-row flex-1">
           <div className="border-primary/5 border-2 max-w-xs flex flex-col gap-2 justify-between shadow-md rounded-md p-4">
-            {/* Section 1 */}
+            {/* Section 1 - Theme Toggle & Mirror Button*/}
             <div className="flex flex-col gap-2">
               <ModeToggle />
-              <Button variant={'outline'} size={'icon'} onClick={() => {setMirrored((mirror) => !mirror)}}><FlipHorizontal /></Button>
-              <Separator />
+              <Button
+                variant={"outline"}
+                size={"icon"}
+                onClick={() => {
+                  setMirrored((mirror) => !mirror);
+                }}
+              >
+                <FlipHorizontal />
+              </Button>
+              <Separator className="my-2" />
             </div>
-            {/* Section 2 */}
+
+            {/* Section 2 - Screenshot & Recording*/}
             <div className="flex flex-col gap-2">
-              <Separator />
-              <Separator />
+              <Separator className="my-2" />
+
+              <Button
+                variant={"outline"}
+                size={"icon"}
+                onClick={userPromptScreenshot}
+              >
+                <CameraIcon />
+              </Button>
+
+              <Button
+                variant={isRecording ? "destructive" : "outline"}
+                size={"icon"}
+                onClick={userPromptRecording}
+              >
+                <Video />
+              </Button>
+
+              <Separator className="my-2" />
+
+              <Button
+                variant={"outline"}
+                size={"icon"}
+                onClick={toggleAutoRecording}
+              ></Button>
             </div>
+
             {/* Section 3 */}
-            <div className="flex flex-col gap-2">
-              {/* <Separator /> */}
-            </div>
+            <div className="flex flex-col gap-2">{/* <Separator /> */}</div>
           </div>
         </div>
       </div>
     </div>
   );
+
+  function userPromptScreenshot() {
+    // take picture
+    // save picture
+  }
+
+  function userPromptRecording() {
+    // check if recording
+    // - then stop recording
+    // - ask for downloading record
+    // if not recording
+    // - start recording
+  }
+
+  function toggleAutoRecording() {}
+
+  // Converting to Text
+  function userPromptListening() {
+    // check if listening
+    // - stop listening
+    // - copy and delete icon appear
+    // if not listening
+    // - delete what've been heard before
+    // - start listening
+  }
+
+  function toggleAutoListening() {}
 }
